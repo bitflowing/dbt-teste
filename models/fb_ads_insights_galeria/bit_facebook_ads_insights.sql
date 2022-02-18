c113d2796a44e4638116f376a495f4dcb45c5df2
ygor-leao <ygor.oliveira@engdb.com.br>
2022-02-18 15:12:23 -0300

    facebook action models_v4

--- models/fb_ads_insights_galeria/bit_facebook_ads_insights.sql
+++ models/fb_ads_insights_galeria/bit_facebook_ads_insights.sql
@@ -15,12 +15,12 @@ SELECT
     JSON_EXTRACT_SCALAR(_airbyte_data, "$.account_currency")  AS account_currency,
     CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.spend") AS FLOAT64)  AS spend,
     CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.impressions") AS INT64)  AS impressions,
-    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.inline_link_clicks") AS INT64)  AS inline_link_clicks
+    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.inline_link_clicks") AS INT64)  AS inline_link_clicks,
     CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.unique_inline_link_clicks") AS INT64)  AS unique_inline_link_clicks,
-    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.unique_clicks") AS INT64)  AS unique_clicks
-    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.clicks") AS INT64)  AS clicks
-    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.frequency") AS INT64)  AS frequency
-    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.reach") AS INT64)  AS reach
+    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.unique_clicks") AS INT64)  AS unique_clicks,
+    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.clicks") AS INT64)  AS clicks,
+    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.frequency") AS INT64)  AS frequency,
+    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.reach") AS INT64)  AS reach,
     CAST(JSON_EXTRACT_SCALAR(_airbyte_data, "$.inline_post_engagement") AS INT64)  AS inline_post_engagement
 FROM 
     bitflowin-nonprod.galeria_airbyte._airbyte_raw_bit_4901315733251836_ads_insights 
